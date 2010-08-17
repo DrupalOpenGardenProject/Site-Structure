c3098419408a0683e9d5f48164fa4b475d744b78
Drupal Open Garden Project <chach@garden.localbiology.org>
2010-08-16 23:24:15 -0500

    removing unnecessary files

--- sites/all/modules/contrib/location/locations.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php if (count($locations)) {?>
-<h3 class="location-locations-header"><?php echo count($locations) > 1 ? t('Locations') : t('Location');?></h3>
-<div class="location-locations-wrapper">
-<?php
-  foreach ($locations as $location) {
-    echo $location;
-  }
-  echo '</div>';
-} ?>
