3ccca139781bc8b1be3c0d23d437606cd7df0489
Drupal Open Garden Project <chach@garden.localbiology.org>
2010-08-29 11:15:19 -0500

    refactored views summary alphabet icons module; it is now 2 modules - one module for glossary titles, another for glossary images; you should disable the old module & enable the two new modules;

---
+++ sites/all/modules/custom/views_glossary_abc/views_glossary_images/theme/views-view-glossary-images.tpl.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default simple view template to display a list of summary lines
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="item-list">
+  <ul class="views-summary">
+  <?php foreach ($rows as $id => $row): ?>
+    <li><a href="<?php print $row->url; ?>"<?php print !empty($classes[$id]) ? ' class="'. $classes[$id] .'"' : ''; ?>><?php print $row->link; ?></a>
+      <?php if (!empty($options['count'])): ?>
+        (<?php print $row->count?>)
+      <?php endif; ?>
+    </li>
+  <?php endforeach; ?>
+  </ul>
+</div>
